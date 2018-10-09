ebb65dc68a605ffec6a407e069b87fc018086fd9
Johnny Kaparrala <30930747+JohnnyKaparrala@users.noreply.github.com>
2018-10-08 22:46:00 -0300

    a

--- sis_line/main.c
+++ sis_line/main.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main() {
 	char opcao;
 	FILE *f;
 	char c = 0;
-	f = fopen("teste.txt", "r");
+	int8_t qtdSistemas= 0;
+	int linha = 0;
+	char* stringSistema;
+    int *coeficientes;
+    char **nomesVariaveis;
+    int qtdLetras = 0;
+    int termo = 0;
 
 	do {
-		while((c=fgetc(f))!=EOF){
-            printf("%c",c);
+        f = fopen("teste.txt", "r");
+	    qtdSistemas = (int8_t)fgetc(f) - '0';// converte o primeiro char para int
+
+        coeficientes = (int *) malloc(sizeof(int) * qtdSistemas);
+        nomesVariaveis = malloc(qtdSistemas * sizeof(char*));
+
+        printf("%d", qtdSistemas);
+
+        stringSistema = malloc(sizeof(char) * 256);
+        int len = 0;
+
+		while ( (c = fgetc(f))!= EOF ) {
+            if (c != ' ' && c != '*') {
+                stringSistema += c;
+                len++;
+            }
         }
+        stringSistema = malloc(sizeof(char) * 256);
+
+        printf("\n%s", stringSistema);
 
 		printf("\nSair do programa? (s/n)\n");
 		scanf(" %c", &opcao);
